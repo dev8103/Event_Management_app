fce250ba5b2e308d5bd8507877f32be87b9322fa
Dev Hingu <devhingu8103@gmail.com>
2024-07-01 01:20:02 +0530

    necessary changes in events

--- BACKEND/controllers/events/getEventbycc.js
+++ BACKEND/controllers/events/getEventbycc.js
@@ -1,8 +1,24 @@
 const Event = require("../../models/eventModel");
+const clubcommittee = require('../../models/ClubCommitteeModel');
 
 const getEventbycc = async (req,res)=>{
     try{
         const email = req.userData.email;
+
+        const curCC = await clubcommittee.findOne({email});
+        try{
+            if(curCC == null){
+                res.status(401);
+                throw new Error("User not Authrised.");
+            }
+        }
+        catch(error){
+            console.log("This error is coming from getEventscc.js/auth part");
+            console.log(error);
+            res.status(401).json({message:error.message});
+            return;
+        }
+
         // console.log(email);
         const curEvents = await Event.find({clubCommitteeEmail:email});
         if(!curEvents){
